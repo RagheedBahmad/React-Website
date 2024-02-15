5a2fc62eb13adadc06b6d19f4c6f6107c2b11fc5
RagheedBahmad <rlb01@aub.edu.lb>
2024-02-15 21:21:49 +0200

    Finished First Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Main from "./components/Main.jsx";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import logo from "../assets/react.svg";
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <img src={logo} alt="React logo" />
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
